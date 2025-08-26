8e2c883cea769aa6d043afb0dfbe9e0c4609a7b0
vongdefu <1129432705@qq.com>
2025-08-26 14:49:38 +0800

    update sidebar

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -4,7 +4,7 @@ export const sidebar: DefaultTheme.Config["sidebar"] = {
   "/vongdefu-sourcecode-hub/": [
     {
       text: "分布式事务",
-      base: "/seata",
+      base: "/vongdefu-sourcecode-hub/seata",
       items: [
         { text: "seata的安装", link: "/seata的安装" },
         { text: "分布式事务tcc模式", link: "/分布式事务tcc模式" },
